af9bea89d3ad013fa69763fee67d7c06ec55b549
dzhankui <dzhankui@bk.ru>
2022-03-21 23:14:52 +0300

    add duration.py

---
+++ duration.py
@@ -0,0 +1,12 @@
+print('Введите период в секундах, а я все сделаю красиво!')
+minutes = 0
+hours = 0
+days = 0
+seconds = 0
+period = int(input())
+
+
+print(days)
+print(hours)
+print(minutes)
+print(seconds)
