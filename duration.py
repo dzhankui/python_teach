85e1821b77e46470e1fd331ac523f59da2ff052b
dzhankui <dzhankui@bk.ru>
2022-03-22 19:18:30 +0300

    solve duration.py

--- duration.py
+++ duration.py
@@ -1,12 +1,30 @@
 print('Введите период в секундах, а я все сделаю красиво!')
-minutes = 0
-hours = 0
-days = 0
-seconds = 0
-period = int(input())
+mins = 0
+hrs = 0
+d = 0
+sec = 0
+duration = int(input())
 
+if duration >= 60:
+    mins = duration // 60
+    sec += duration % 60
+    hrs = mins // 60
+    mins = mins % 60
+    d = hrs // 24
+    hrs = hrs % 24
+    print(d, 'дн', hrs, 'ч', mins, 'мин', 'и', sec, 'сек')
 
-print(days)
-print(hours)
-print(minutes)
-print(seconds)
+else:
+    print('вы веели лишь', duration, 'сек')
+
+# if duration >= 60:
+#     mins = duration // 60 #sec
+#     sec += duration%60
+#     hrs = mins // 60 #min
+#     a = 3 % 5
+#     d = hrs // 24 #hrs
+#     sec = a
+# elif duration < 60:
+#     print('вы ввели', duration, 'секунд')
+# print('duration = ', duration, 'сек')
+# print(d, 'дн', hrs, 'ч', mins, 'мин', 'и', sec, 'сек')
