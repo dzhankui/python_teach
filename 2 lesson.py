8719afd6de530b1c625ccc6c42bf282b313ede0b
dzhankui <dzhankui@bk.ru>
2022-04-08 11:43:19 +0300

    Words.py

--- 2 lesson.py
+++ 2 lesson.py
@@ -1,21 +1,44 @@
-# print(type(15*3))
-# print(type(15/3))
-# print(type(15//3))
-# print(type(15**2))
-# a = (0, 'wee', [1, 2, 3],'er')
-# print(type(a))
+print(type(15*3))
+print(type(15/3))
+print(type(15//3))
+print(type(15**2))
+
+#todo split '5' and '+' done
+#todo add zero to 5 and 5 to get 05
+#todo add '+' to the second 05 => +05
+#todo add " before and after numbers
 
 my_list = ['в ', '5', ' часов ', '17', ' минут ', 'температура ', 'воздуха ', 'была ', '+5', ' градусов']
+print(my_list.index('+5'))
+cut_list = my_list[8]
+cut_list = my_list[8].split('+', 1)
+print(cut_list)
+plus = '+'
+quotes = '"'
+plus5 = plus + cut_list[1]
+# print(plus5)
+quotes = '"'
+
+information = f'в {quotes}{my_list[1]:0>2}{quotes} часов {quotes}{my_list[3]}{quotes} минут температура воздуха была {quotes}{plus}{cut_list[1]:0>2}{quotes} градусов'
+print(information)
+#тупое обособление кавычками
 # my_list.insert(1, '"')
 # my_list.insert(3, '"')
 # my_list.insert(5, '"')
 # my_list.insert(7, '"')
 # my_list.insert(12, '"')
 # my_list.insert(14, '"')
-my_list.insert(my_list.index('+5'), '"')
-my_list.insert(my_list.index('+5')+1, '"')
-# my_list[3].extend(['"'])
-# my_list[1].extend(['"'])
-# print(my_list.index('+5'))
-print(my_list)
-print(''.join(my_list))
+
+# не менее тупо, но с привязкой к индексу элемента
+# my_list.insert(my_list.index('+5'), '"')
+# my_list.insert(my_list.index('+5') + 1, '"')
+# my_list.insert(my_list.index('5'), '"')
+# my_list.insert(my_list.index('5') + 1, '"')
+# my_list.insert(my_list.index('17'), '"')
+# my_list.insert(my_list.index('17') + 1, '"')
+# print(my_list)
+
+# print(''.join(my_list))
+
+
+
--- Words.py
+++ Words.py
@@ -10,26 +10,14 @@
 # процента 2 3 4 23 33 43 44 53 54 63 64 73 74 83 84 93 94
 # процентов 1 2 3 4 5 6 7 8 9 x10 10+
 # починить все
-# num = []
-# word = ''
-# for i in range(1, 101):
-#     num.append(i)
-#     if (i % 2 == 0) and (i % 5 == 0) and (4 <= i >= 10):
-#         word = 'процентов'
-#     if (i % 3 == 0) and (i % 4 == 0) and (i % 5 == 0):
-#         word = 'процент'
-#     elif  #create a condition, that check last digit of a num
-#
-#     print(i, word)
-percent = int(input('Введите количество процентов'))
-word = '' # процент процента процентов
-if percent <= 100:
-    if percent == 1:
-        word = 'процент'
-        print(percent, word)
-    elif percent <= 4:
-        word = 'процента'
-        print(percent, word)
-    else:
-        word = 'процентов'
-        print(percent, word)
+num = []
+word = ''
+for i in range(1, 101):
+     num.append(i)
+     if (i % 2 == 0) and (i % 5 == 0) and (4 <= i >= 10):
+         word = 'процентов'
+     if (i % 3 == 0) and (i % 4 == 0) and (i % 5 == 0):
+         word = 'процент'
+     #elif create a condition, that check last digit of a num
+
+     print(i, word)
